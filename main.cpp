9b92346401c2471b83f2614e34d948c6de75f555
Faiz Rizqi Aqila Lubis <faizrizqialrusyd@gmail.com>
2026-04-02 14:42:12 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(int argc, char ** argv) {
     string name;
-    cout << "Enter your name: " << endl;
+    cout << "Enter your name please: " << endl;
     getline(cin, name);
     cout << "Halo, " << name << "!" << endl;
 
     return 0;
-}
+} 
